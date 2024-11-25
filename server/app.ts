768f9424e0abbf81a244161988e6d8cff6e67a11
Arzy <arzy@dhcp-10-29-242-2.dyn.MIT.EDU>
2024-11-25 16:32:08 -0500

    initiated requesting

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,11 @@
 import AuthenticatingConcept from "./concepts/authenticating";
-import SessioningConcept from "./concepts/sessioning";
 import ListingConcept from "./concepts/listing";
+import RequestingConcept from "./concepts/requesting";
+import SessioningConcept from "./concepts/sessioning";
 
 // The app is a composition of concepts instantiated here
 // and synchronized together in `routes.ts`.
 export const Sessioning = new SessioningConcept();
 export const Authing = new AuthenticatingConcept("users");
 export const Listing = new ListingConcept("listings");
-
+export const Requesting = new RequestingConcept("requests");
