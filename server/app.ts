4bb50458de9d3040f220024f245f465b657f0e78
Carla Duong <cduong@mit.edu>
2024-12-02 01:23:25 -0500

    offering concept added

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import AuthenticatingConcept from "./concepts/authenticating";
 import ClaimingConcept from "./concepts/claiming";
 import ListingConcept from "./concepts/listing";
+import OfferingConcept from "./concepts/offering";
 import RequestingConcept from "./concepts/requesting";
 import SessioningConcept from "./concepts/sessioning";
 
@@ -11,4 +12,5 @@ export const Authing = new AuthenticatingConcept("users");
 export const Listing = new ListingConcept("listings");
 export const Requesting = new RequestingConcept("requests");
 export const Claiming = new ClaimingConcept("claims");
+export const Offering = new OfferingConcept("offers");
 //   }
